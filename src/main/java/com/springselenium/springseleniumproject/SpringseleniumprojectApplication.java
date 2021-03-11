7065b567b840040ee2edb0a79aee9b07e91b9113
karthicksubramanian <crownkarthick@gmail.com>
2021-03-10 21:55:52 -0800

    1st spring selenium commit

---
+++ src/main/java/com/springselenium/springseleniumproject/SpringseleniumprojectApplication.java
@@ -0,0 +1,13 @@
+package com.springselenium.springseleniumproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringseleniumprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringseleniumprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springselenium/springseleniumproject/SpringseleniumprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springselenium.springseleniumproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringseleniumprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
