1ee1b69bef72a731ff453f57d7d8d8da5f9720b8
karthicksubramanian <crownkarthick@gmail.com>
2021-04-01 13:01:58 -0700

    "Added dependency and properties to POM.xml"

--- src/test/java/com/springselenium/springseleniumproject/SpringseleniumprojectApplicationTests.java
+++ src/test/java/com/springselenium/springseleniumproject/SpringseleniumprojectApplicationTests.java
@@ -1,13 +1,19 @@
 package com.springselenium.springseleniumproject;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("/application-test.properties")
 class SpringseleniumprojectApplicationTests {
+	@Value("${welcome.message}")
+	private String welcomeMessage;
 
 	@Test
 	void contextLoads() {
+		System.out.println(welcomeMessage);
 	}
 
 }
