ff37c6b0f44ef38fb88141a094589b280ffe0d3f
karthicksubramanian <crownkarthick@gmail.com>
2021-04-02 10:53:50 -0700

    "Added BrowserFactory enum"

--- src/main/java/com/springselenium/springseleniumproject/SpringseleniumprojectApplication.java
+++ src/main/java/com/springselenium/springseleniumproject/SpringseleniumprojectApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringseleniumprojectApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringseleniumprojectApplication.class, args);
 	}
+
+
 
 }
---
+++ src/main/java/driver/BrowserFactory.java
@@ -0,0 +1,72 @@
+package driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.config.DriverManagerType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.AbstractDriverOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("/application.properties")
+public enum BrowserFactory {
+
+    CHROME {
+        @Override
+        public WebDriver createDriver() {
+            WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
+            return new ChromeDriver(getOptions());
+        }
+
+        @Override
+        public ChromeOptions getOptions() {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments(START_MAXIMIZED);
+            chromeOptions.addArguments("--disable-infobars");
+            chromeOptions.addArguments("--disable-notifications");
+            chromeOptions.setHeadless(Boolean.parseBoolean(getMessage()));
+            return chromeOptions;
+        }
+
+    },
+    FIREFOX {
+        @Override
+        public WebDriver createDriver() {
+            WebDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
+            return new FirefoxDriver(getOptions());
+        }
+
+        @Override
+        public FirefoxOptions getOptions() {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.addArguments(START_MAXIMIZED);
+            firefoxOptions.setHeadless(Boolean.parseBoolean(getMessage()));
+
+            return firefoxOptions;
+        }
+    };
+
+    private static final String START_MAXIMIZED = "--start-maximized";
+
+    public abstract WebDriver createDriver();
+
+    public abstract AbstractDriverOptions<?> getOptions();
+
+    @Value("${headless}")
+    private static String message;
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+
+}
